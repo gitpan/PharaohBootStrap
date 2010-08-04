f946b9a9417471343763d5e496c06aa75ccfd517
Dmytro Zagashev <zdm@cpan.org>
2010-08-04 07:29:13 +0000

    Import of ZDM/PharaohBootStrap-3.00 from CPAN.
    gitpan-cpan-distribution: PharaohBootStrap
    gitpan-cpan-version:      3.00
    gitpan-cpan-path:         ZDM/PharaohBootStrap-3.00.tar.gz
    gitpan-cpan-author:       ZDM
    gitpan-cpan-maturity:     released

---
+++ lib/PharaohBootStrap.pm
@@ -0,0 +1,22 @@
+package Pharaoh::BootStrap 3.00;
+
+use 5.12.0;
+use warnings;
+use utf8;
+use open IO => ':utf8';
+
+BEGIN {
+    use FindBin qw($Bin $Script);
+    $0 = $Bin . '/' . $Script;
+    chdir $Bin;
+    chdir($main::project_path) if $main::project_path;
+
+    if (!$main::pharaoh_path && $main::pharaoh_config && -e $main::pharaoh_config) {
+        $main::pharaoh_path = do $main::pharaoh_config || die $@;
+    }
+
+    push @INC, ('lib/') if -e 'lib/';
+    push @INC, ($main::pharaoh_path) if -e $main::pharaoh_path;
+}
+
+1;
